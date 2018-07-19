6acbe3747850e15a058f8f7c912723913f1ae59c
Senthilkumar Gopal <sengopal@ebay.com>
2018-07-18 21:16:06 -0700

    test commit

---
+++ src/test/java/com/sengopal/boilerplate/ChromeDriverTest.java
@@ -0,0 +1,38 @@
+package com.sengopal.boilerplate;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class ChromeDriverTest {
+
+    @Test
+    public void testDriverLoad() throws InterruptedException {
+        // Optional, if not specified, WebDriver will search your path for chromedriver.
+//        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com/");
+        Thread.sleep(5000);
+
+        WebElement userId = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("input[type='text']"))));
+
+        userId.sendKeys("travis-ci");
+        driver.findElement(By.name("btnK")).submit();
+
+        Thread.sleep(5000);
+
+        String url = null;
+        if (driver.getCurrentUrl().contains("https://www.google.com/search")) {
+            System.out.println("Code Obtained");
+        }
+        driver.quit();
+    }
+}
