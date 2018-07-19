0186d0207bd656a3e293a43db633447629c97f16
Senthilkumar Gopal <sengopal@ebay.com>
2018-07-18 22:28:09 -0700

    adding file - 6

--- src/test/java/com/sengopal/boilerplate/ChromeDriverTest.java
+++ src/test/java/com/sengopal/boilerplate/ChromeDriverTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,7 +17,9 @@ public class ChromeDriverTest {
         // Optional, if not specified, WebDriver will search your path for chromedriver.
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox");
+        WebDriver driver = new ChromeDriver(chromeOptions);
 
         driver.get("https://www.google.com/");
         Thread.sleep(5000);
